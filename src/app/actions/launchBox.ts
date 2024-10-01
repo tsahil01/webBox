702cc029eb68181619ca6d466009723f6f7595ce
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-10-01 21:00:11 +0530

    MAJOR: launchBox

---
+++ src/app/actions/launchBox.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { createContainer } from "@/config/docker";
+import { NEXTAUTH_CONFIG } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+
+export async function launchBox(imgName: string, cmd: string) {
+    const session = await getServerSession(NEXTAUTH_CONFIG);
+    console.log(session);
+    let containerId;
+
+    if (session) {
+      try {
+        const techStack = await prisma.techStack.findFirst({
+          where: {
+            imageName: imgName,
+          },
+        });
+
+        if (!techStack) {
+          console.log("Tech Stack not found");
+          return;
+        }
+
+        // console.log("TechStack: ", techStack);
+
+        containerId = await createContainer(imgName);
+        console.log(containerId);
+
+        // add data in container table =>
+
+        if (session?.userId) {
+            console.log("user: ", session.userId);
+          const createContainer = await prisma.container.create({
+            data: {
+              userId: session?.userId,
+              techStackId: techStack?.id,
+              status: "started",
+              containerName: imgName, // TODO: add container name from docker.ts
+              containerId: containerId,
+            },
+          });
+        }
+      } catch (e) {
+        console.log("Unable to launch Container: ", e);
+        return;
+      }
+      if (containerId) {
+        redirect(`/playground/${containerId}`);
+        return;
+      }
+    } else {
+      console.log("No user logged in");
+    }
+  }
