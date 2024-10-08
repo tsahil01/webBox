abe9f3c5c4070e37f04b71ad5458e6b17eeddf2c
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-10-08 23:23:56 +0530

    term remove non req code

--- src/components/term.tsx
+++ src/components/term.tsx
@@ -9,14 +9,10 @@ export default function Terminal({
   containerId: string | null;
 }) {
   return (
-    <div className="bg-zinc-950 rounded-md flex flex-col gap-3 p-3 overflow-auto">
-      <div className="flex gap-2 my-auto border-b mb-4 p-2 justify-center">
-        <TerminalIcon className="w-9 h-9 text-white" />
-        <div className="text-4xl font-bold text-white my-auto">Web - Terminal</div>
-      </div>
+    <div className="bg-zinc-950 rounded-md flex flex-col p-3 h-full overflow-auto">
       <div
-        className="bg-black rounded-lg shadow-lg w-full h-full  scroll-m-0"
-        ref={terminalRef} // Set explicit dimensions
+        className="bg-black rounded-lg shadow-lg w-full flex-grow scroll-m-0"
+        ref={terminalRef}
       />
     </div>
   );
